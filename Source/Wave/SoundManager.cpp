f32489a1fd440712ae4cd3d33aaebf01fbe59715
ryouhei <ryouhei05205005@gmail.com>
2020-04-20 00:44:29 +0900

    複数の音声を同時に再生できるように変更

--- Source/Wave/SoundManager.cpp
+++ Source/Wave/SoundManager.cpp
@@ -6,18 +6,44 @@ static ASoundManager* SoundManagerInstance = nullptr;
 // Sets default values
 ASoundManager::ASoundManager()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("ModifySkeletalMeshComponent"));
+
 	SoundManagerInstance = this;
+}
+
+void ASoundManager::BeginPlay() {
+	if (AudioComponent.Num() == SoundList.Num())
+	{
+		return;
+	}
+
+	for (const auto& Cue : SoundList)
+	{
+		FString CueName = Cue->GetName();
+		FName AudioCompName(*CueName);
+
+		// create AudioComponent
+		auto NewAudioComponent = NewObject<UAudioComponent>(this, AudioCompName);
+
+		AudioComponent.Add(NewAudioComponent);
+	}
 }
 
 void ASoundManager::PlaySound(int Index)
 {
 	if (Index < 0 || Index >= SoundList.Num()) return;//リストに存在しないインデックスなら再生しない
 	if (SoundList.GetData()[Index] == nullptr) return;//サウンドが設定されていないなら再生しない
-	AudioComponent->Sound = SoundList.GetData()[Index];//サウンド設定
-	AudioComponent->Play();//サウンドを再生する
+
+	AudioComponent[Index]->Sound = SoundList.GetData()[Index];//サウンド設定
+	AudioComponent[Index]->Play();//サウンドを再生する
+	AudioComponent[Index]->SetPaused(true);
+}
+
+void ASoundManager::StopSound(int Index) {
+	if (Index < 0 || Index >= SoundList.Num()) return;//リストに存在しないインデックスなら再生しない
+	if (SoundList.GetData()[Index] == nullptr) return;//サウンドが設定されていないなら再生しない
+
+	AudioComponent[Index]->Stop();//サウンドを停止する
 }
 
 ASoundManager* ASoundManager::GetInstance()
@@ -29,4 +55,10 @@ void ASoundManager::SafePlaySound(int Index)
 {
 	if (!SoundManagerInstance) return;
 	SoundManagerInstance->PlaySound(Index);
+}
+
+void ASoundManager::SafeStopSound(int Index)
+{
+	if (!SoundManagerInstance) return;
+	SoundManagerInstance->StopSound(Index);
 }
--- Source/Wave/SoundManager.h
+++ Source/Wave/SoundManager.h
@@ -13,21 +13,32 @@ class WAVE_API ASoundManager : public AActor
 	GENERATED_BODY()
 private:
 	UPROPERTY(EditAnywhere)
-	UAudioComponent* AudioComponent = nullptr;//音声コンポーネントクラス
-
+		TArray <UAudioComponent*> AudioComponent;
+	
 	UPROPERTY(EditAnywhere, category = "SoundSetList")
 	TArray <USoundBase*> SoundList;//再生する音声のリスト
 public:	
 
 	ASoundManager();
 
+	virtual void BeginPlay() override;
+
 	//@brief 音声を再生する
 	//@param Index [in] 再生する音声のインデックス
 	void PlaySound(int Index);
 
+	//@brief 音声を停止する
+	//@param Index [in] 停止する音声のインデックス
+	void StopSound(int Index);
+
 	//@brief 再生可能かチェックを行い、可能なら音声を再生する
 	//@param Index [in] 再生する音声のインデックス
 	static void SafePlaySound(int Index);
+
+	//@brief 停止可能かチェックを行い、可能なら音声を停止する
+	//@param Index [in] 停止する音声のインデックス
+	static void SafeStopSound(int Index);
+
 
 	//@brief サウンドマネージャを取得する
 	//@return サウンドマネージャへのアドレス
