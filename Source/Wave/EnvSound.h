c8380bcb2e8396fdf01f087ae8dca4604df43025
ryouhei <ryouhei05205005@gmail.com>
2020-05-19 00:44:01 +0900

    環境音を鳴らすクラスを作成

---
+++ Source/Wave/EnvSound.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Actor.h"
+#include "EnvSound.generated.h"
+
+UCLASS()
+class WAVE_API AEnvSound : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnvSound();
+	UPROPERTY(EditAnywhere)
+	USoundBase* soundBase;
+
+	UPROPERTY(EditAnywhere)
+	FTransform transform;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	void Play3DSound();
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
