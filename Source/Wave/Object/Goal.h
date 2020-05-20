b5d731b2b2fa8d27ea25394b9f98f760a0969fe0
YanaseTaiho <taiho0423@gmail.com>
2020-05-20 18:11:31 +0900

    ゲームクリア条件を変更＆ゴールのアニメーションを追加

--- Source/Wave/Object/Goal.h
+++ Source/Wave/Object/Goal.h
@@ -2,20 +2,18 @@
 
 #pragma once
 // ゴールクラス
-// オブジェクトが触れたらゴールUIを表示する
+
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/SphereComponent.h"
 #include "Goal.generated.h"
-class UHammerCountUI;
+
 UCLASS()
 class WAVE_API AGoal : public AActor
 {
 	GENERATED_BODY()
 private:
-	// 表示するUI　エディタで指定する
-	UPROPERTY(EditAnywhere)
-		TSubclassOf<UHammerCountUI> UIClass;
+
 public:
 	// Sets default values for this actor's properties
 	AGoal();
@@ -23,29 +21,27 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	//ハンマーを叩ける回数
-	UPROPERTY(EditAnywhere, Category = "Instanced")
-		int HammerCount = 0;
+
 	UPROPERTY()
 		USceneComponent* Scene;
-
+	// 荷物の衝突を判断するコリジョン
 	UPROPERTY(EditAnywhere, BluePrintReadWrite)
 		USphereComponent* SphereComp;
-	UPROPERTY(BlueprintReadWrite, Category = "C++Class")
-		bool isGoal;
+
 	//OnComponentBeginOverlap
 	UFUNCTION()
 		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	// ドアを閉めるアニメーションをブループリント側で作成
+	UFUNCTION(Category = "C++Event", BlueprintImplementableEvent, BlueprintCallable)
+		void PlayAnimationDoorClose();
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
 	UFUNCTION(BlueprintCallable, Category = "C++Library")
-		UUserWidget* WidgetCreate(TSubclassOf<UUserWidget> UserWidgetClass);
-	UFUNCTION(BlueprintCallable, Category = "C++Library")
-		FORCEINLINE bool GetIsGoal() const { return isGoal; }
-	void MinusHammerCount();
+		bool GetIsGoal() const { return isGoal; }
 private:
-	UHammerCountUI* HammerCountUI;
-	//ハンマーを叩ける回数が1以上ならUIを生成
-	void CreateHammerCountUI();
+	bool isGoal;
+
 };
