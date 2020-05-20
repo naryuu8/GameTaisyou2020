06b1b9b1b1d12790487d6ef067ada9d361bad590
namai.ryuuki <namai.ryuuki@gmail.com>
2020-05-20 14:03:09 +0900

    鉄砲水を作成

---
+++ Source/Wave/WaterSurface/FlashFlood.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FlashFlood.generated.h"
+
+UCLASS()
+class WAVE_API AFlashFlood : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFlashFlood();
+
+	float GetXLength() { return XLength; }
+	float GetYLength() { return YLength; }
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual bool ShouldTickIfViewportsOnly() const override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FVector GetFloatVec(FVector worldPos);
+
+private:
+	UPROPERTY(EditAnywhere)
+		float XLength = 100.0f;
+	UPROPERTY(EditAnywhere)
+		float YLength = 100.0f;
+	UPROPERTY(EditAnywhere)
+		float Speed = 100.0f;
+	UPROPERTY(EditAnywhere)
+		FVector FloatVec;
+};
