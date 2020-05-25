c99ae4d2315015d544627c6008dd609660e78e0e
OG <orengi93@yahoo.co.jp>
2020-05-25 15:28:56 +0900

    ゲームオーバーUI作成

--- Source/Wave/UI/GameOverUI.cpp
+++ Source/Wave/UI/GameOverUI.cpp
@@ -3,3 +3,53 @@
 
 #include "GameOverUI.h"
 
+FSlateColor UGameOverUI::SelectTextColor(const GameOverState state)
+{
+	if (SelectNumber == static_cast<int>(state))
+	{
+		return FSlateColor(FLinearColor(1.0f, 0.0f, 0.0f, 1.0f));
+	}
+	return FSlateColor(FLinearColor(1.0f, 1.0f, 1.0f, 1.0f));
+}
+
+ESlateVisibility UGameOverUI::GetGameOverTextVisibility()
+{
+	if (IsPlayAnimation)
+	{
+		return ESlateVisibility::Hidden;
+	}
+	return ESlateVisibility::Visible;
+}
+
+void UGameOverUI::NextSelectState()
+{
+	if (IsPlayAnimation)return;
+	SelectNumber++;
+	if (SelectNumber > static_cast<int>(GameOverState::STAGESELECT))
+	{
+		SelectNumber = static_cast<int>(GameOverState::RESTART);
+	}
+}
+
+void UGameOverUI::BackSelectState()
+{
+	if (IsPlayAnimation)return;
+	SelectNumber--;
+	if (SelectNumber < 0)
+	{
+		SelectNumber = static_cast<int>(GameOverState::STAGESELECT);
+	}
+}
+
+void UGameOverUI::SelectStateAction()
+{
+	if (IsPlayAnimation)return;
+	switch (SelectNumber)
+	{
+		case static_cast<int>(GameOverState::RESTART) :
+			EndPlayAnimation();
+				break;
+			case static_cast<int>(GameOverState::STAGESELECT) :
+				break;
+	}
+}
