46c10021950f8fddad29cf3ffe6df14f6d52a8c4
OG <orengi93@yahoo.co.jp>
2020-06-03 10:15:29 +0900

    ゲームオーバー時にプレイヤーモデル表示

---
+++ Source/Wave/UI/UISceneCapture2D.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/SceneCapture2D.h"
+#include "UISceneCapture2D.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WAVE_API AUISceneCapture2D : public ASceneCapture2D
+{
+	GENERATED_BODY()
+};
