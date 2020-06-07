c5e736ede7e7727956fa11677e6f16721d7331d0
namai.ryuuki <namai.ryuuki@gmail.com>
2020-06-08 08:04:32 +0900

    サウンドのバグを修正

--- Source/Wave/SoundManager.cpp
+++ Source/Wave/SoundManager.cpp
@@ -77,6 +77,5 @@ UAudioComponent* ASoundManager::CreateAudioComponent(SOUND_TYPE type) {
 	auto NewAudioComponent = NewObject<UAudioComponent>(SoundManagerInstance);
 	TCHAR* fileName = FileNames[(int)type];
 	NewAudioComponent->Sound = LoadObject<USoundBase>(NULL, fileName, NULL, LOAD_None, NULL);
-	SoundManagerInstance->AddAudioComponent(NewAudioComponent);
 	return NewAudioComponent;
 }
