71cc007df539b7a26a88bf079d4b91da1f53a290
OG <orengi93@yahoo.co.jp>
2020-07-23 11:31:44 +0900

    対戦モード時のリザルト画面作成中

--- Source/Wave/UI/BattleResultUI.cpp
+++ Source/Wave/UI/BattleResultUI.cpp
@@ -2,4 +2,42 @@
 
 
 #include "BattleResultUI.h"
+#include "../InputManager.h"
+#include "../SoundManager.h"
 
+void UBattleResultUI::NextSelectState()
+{
+	SelectNumber++;
+	if (SelectNumber > static_cast<int>(BattleResultState::STAGESELECT))
+	{
+		SelectNumber = static_cast<int>(BattleResultState::RESTART);
+	}
+	ASoundManager::SafePlaySound(SOUND_TYPE::MENU_SELECT);
+	ImageSizeChenge();
+}
+
+void UBattleResultUI::BackSelectState()
+{
+	SelectNumber--;
+	if (SelectNumber < static_cast<int>(BattleResultState::RESTART))
+	{
+		SelectNumber = static_cast<int>(BattleResultState::STAGESELECT);
+	}
+	ASoundManager::SafePlaySound(SOUND_TYPE::MENU_SELECT);
+	ImageSizeChenge();
+}
+
+void UBattleResultUI::SelectStateAction()
+{
+	switch (SelectNumber)
+	{
+		case static_cast<int>(BattleResultState::RESTART) :
+			RetryScene();
+			ASoundManager::SafePlaySound(SOUND_TYPE::MENU_DECISION);
+			break;
+			case static_cast<int>(BattleResultState::STAGESELECT) :
+				ASoundManager::SafePlaySound(SOUND_TYPE::MENU_DECISION);
+				StageSelectChenge();
+				break;
+	}
+}
--- Source/Wave/UI/BattleResultUI.h
+++ Source/Wave/UI/BattleResultUI.h
@@ -9,14 +9,36 @@
 /**
  * 
  */
+UENUM(BlueprintType)
+enum class BattleResultState : uint8
+{
+	RESTART,
+	STAGESELECT,
+};
 UCLASS()
 class WAVE_API UBattleResultUI : public UUserWidget
 {
 	GENERATED_BODY()
+private:
+	int SelectNumber = 0;
+	bool IsNoInput = false;//true時インプットを受け付けない
 public:
-	UFUNCTION(Category = "C++Event", BlueprintImplementableEvent, BlueprintCallable)
-		void SetPlayerScore(const int index,const int score);
 	//1Pが勝ったらtrue
 	UFUNCTION(Category = "C++Event", BlueprintImplementableEvent, BlueprintCallable)
 		void SetWinPlayer(const bool player_one_win);
+	void NextSelectState();
+	void BackSelectState();
+	//対応する選択ステートのアクションを実行する
+	void SelectStateAction();
+	UFUNCTION(BlueprintCallable, Category = "C++Library")
+		FORCEINLINE	int GetSelectNumber() const { return SelectNumber; };
+	//キーを押したとき項目画像サイズを変更
+	UFUNCTION(Category = "C++Event", BlueprintImplementableEvent, BlueprintCallable)
+		void ImageSizeChenge();
+	//フェードインを行いリトライする
+	UFUNCTION(Category = "C++Event", BlueprintImplementableEvent, BlueprintCallable)
+		void RetryScene();
+	//フェードインを行いステージ選択画面に戻る
+	UFUNCTION(Category = "C++Event", BlueprintImplementableEvent, BlueprintCallable)
+		void StageSelectChenge();
 };
